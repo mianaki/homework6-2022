cec2a1ab8258b1dbc4642062f08525e77797ea65
mianaki <73906236+mianaki@users.noreply.github.com>
2022-04-14 19:31:33 -0400

    Add files via upload

---
+++ hw7-2021/js/video.js
@@ -0,0 +1,20 @@
+var video = document.getElementById("#play")
+
+document.querySelector("#play").addEventListener("click", function() {
+});
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+video.onclick = function(){
+	video.play();
+}
+
+// document.querySelector("#play").addEventListener("click", function() {
+// });
+
+
+
+
